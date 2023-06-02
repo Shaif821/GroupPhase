48bcd05888fb5acc4b25252e727c061c04ccb2ea
Shaif Bhaggoe <shaif.bhaggoe@gmail.com>
2023-06-02 12:30:10 +0200

    added dependencies to AppModule

--- app/src/main/java/com/example/groupphase/data/data_source/database/AppRoomDatabase.kt
+++ app/src/main/java/com/example/groupphase/data/data_source/database/AppRoomDatabase.kt
@@ -1,4 +1,21 @@
 package com.example.groupphase.data.data_source.database
 
-class AppRoomDatabase {
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.example.groupphase.data.data_source.dao.SimulationDAO
+import com.example.groupphase.domain.model.Simulation
+
+@Database(entities = [Simulation::class], version = 1, exportSchema = false)
+@TypeConverters(TypeConverter::class)
+abstract class AppRoomDatabase : RoomDatabase() {
+
+        abstract fun simulationDao(): SimulationDAO
+
+        companion object {
+            const val DATABASE_NAME = "SIMULATION_DATABASE"
+
+            @Volatile
+            private var INSTANCE: AppRoomDatabase? = null
+        }
 }
--- app/src/main/java/com/example/groupphase/di/AppModule.kt
+++ app/src/main/java/com/example/groupphase/di/AppModule.kt
@@ -1,10 +1,68 @@
 package com.example.groupphase.di
 
+import android.app.Application
+import androidx.room.Room
+import com.example.groupphase.data.data_source.dao.SimulationDAO
+import com.example.groupphase.data.data_source.database.AppRoomDatabase
+import com.example.groupphase.data.repository.SimulationRepositoryImpl
+import com.example.groupphase.domain.repository.SimulationRepository
+import com.example.groupphase.domain.use_case.simulation_use_cases.DeleteSimulationUseCase
+import com.example.groupphase.domain.use_case.simulation_use_cases.GetAllSimulationUseCase
+import com.example.groupphase.domain.use_case.simulation_use_cases.GetSimulationByDateUseCase
+import com.example.groupphase.domain.use_case.simulation_use_cases.GetSimulationByIdUseCase
+import com.example.groupphase.domain.use_case.simulation_use_cases.GetSimulationByWeekUseCase
+import com.example.groupphase.domain.use_case.simulation_use_cases.InsertSimulationUseCase
+import com.example.groupphase.domain.use_case.simulation_use_cases.SimulationUseCases
+import com.example.groupphase.domain.use_case.simulation_use_cases.UpdateSimulationUseCase
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
 @Module
 object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideDatabase(app: Application) : AppRoomDatabase {
+        return Room.databaseBuilder(
+            app,
+            AppRoomDatabase::class.java,
+            AppRoomDatabase.DATABASE_NAME
+        )
+            .fallbackToDestructiveMigration()
+            .build(
+        )
+    }
+
+    @Provides
+    @Singleton
+    fun provideSimulationDao(database: AppRoomDatabase) : SimulationDAO {
+        return database.simulationDao(
+        )
+    }
+
+    @Provides
+    @Singleton
+    fun provideSimulationRepository(simulationDAO: SimulationDAO) : SimulationRepository {
+        return SimulationRepositoryImpl(
+            simulationDAO
+        )
+    }
+
+    @Provides
+    @Singleton
+    fun provideGetAllSimulationsUseCase(simulationRepository: SimulationRepository) : SimulationUseCases {
+        return SimulationUseCases(
+            deleteSimulationUseCase = DeleteSimulationUseCase(simulationRepository),
+            insertSimulationUseCase = InsertSimulationUseCase(simulationRepository),
+            updateSimulationUseCase = UpdateSimulationUseCase(simulationRepository),
+            getAllSimulationUseCase = GetAllSimulationUseCase(simulationRepository),
+            getSimulationByDateUseCase = GetSimulationByDateUseCase(simulationRepository),
+            getSimulationByWeekUseCase = GetSimulationByWeekUseCase(simulationRepository),
+            getSimulationById = GetSimulationByIdUseCase(simulationRepository),
+        )
+    }
 }
